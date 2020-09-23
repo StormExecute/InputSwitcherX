a99959541530b394b555864b4d625a1297003c13
Ghost <vladimirvsevolodovi@yandex.ru>
2020-09-23 21:15:08 +0300

    git: add createPatch core

---
+++ createPatch.py
@@ -0,0 +1,78 @@
+from sys import exit as sysExit
+from binascii import unhexlify
+from os import path as osPath, makedirs as osMakedirs
+
+def make_dirs(dest):
+    if not osPath.exists(dest):
+        osMakedirs(dest)
+
+with open('./input/InputSwitch.dll', 'rb') as f:
+
+    hexdata = f.read().hex()
+    
+    i = 0
+    pointer = 0
+    hexAsList = []
+    
+    for h in hexdata:
+
+        if i % 2 == 0 and i != 0: pointer += 1
+        
+        if not pointer < len(hexAsList): hexAsList.append(h)
+        else: hexAsList[pointer] += h
+        
+        i += 1
+    
+    i = 0
+    inARow = 0
+    inNopSet = 0
+    res = 0
+    
+    for h in hexAsList:
+    
+        if inARow == 6 and inNopSet >= 29:
+            break
+        elif inARow == 6 and h != "33":
+            hexAsList[i] = "90"
+            inNopSet += 1
+            #28 last here
+        elif inARow == 6 and h == "33":
+            if inNopSet == 28: res = 1
+            break
+        elif h == "74" and inARow == 0:
+            inARow += 1
+        elif h == "1f" and inARow == 1:
+            inARow += 1
+        elif h == "48" and inARow == 2:
+            inARow += 1
+        elif h == "63" and inARow == 3:
+            inARow += 1
+        elif h == "d0" and inARow == 4:
+            inARow += 1
+        elif inARow == 5:
+            
+            hexAsList[i] = "90"
+            hexAsList[i - 1] = "90"
+            hexAsList[i - 2] = "90"
+            hexAsList[i - 3] = "90"
+            hexAsList[i - 4] = "90"
+            hexAsList[i - 5] = "90"
+            inNopSet = 1
+            inARow = 6
+            
+        else:
+            inARow = 0
+            
+        i += 1
+        
+    if res == 0:
+        print("Cant patch dll!")
+        sysExit(1)
+        
+    make_dirs("./output")
+        
+    with open('./output/InputSwitch.dll', 'wb') as fout:
+        for h in hexAsList:
+            fout.write(unhexlify(h))
+            
+    print("Done!")
